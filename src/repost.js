422d3a8f92bc459bc62c6651d463d5b4c489d519
Wiliam Joaquim <wiliam.joaquim@tempocerto.inf.br>
2024-09-07 15:05:28 -0300

    remove unused files

--- src/repost.js
+++
@@ -1,3 +0,0 @@
-export default async function repost(agent, uri, cid) {
-    await agent.repost(uri, cid);
-}
--- src/repost.ts
+++
@@ -1,9 +0,0 @@
-import { AtpAgent } from "@atproto/api";
-
-export default async function repost(
-  agent: AtpAgent,
-  uri: string,
-  cid: string
-): Promise<void> {
-  await agent.repost(uri, cid);
-}
