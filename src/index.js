88414603ec361b234b95a7baa769cf3266ec0efc
Wiliam Joaquim <wiliam.joaquim@tempocerto.inf.br>
2024-09-07 16:05:54 -0300

    remove ds store

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-import startBot from "./bot.js";
-import server from "./server.js";
-dotenv.config();
-console.log("Start bot");
-startBot().catch((err) => {
-    console.error("Failed to start the bot", err);
-});
-server();
