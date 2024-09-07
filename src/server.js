53903c39ad4e84a8641de34f81179ceeea606f8d
Wiliam Joaquim <wiliam.joaquim@tempocerto.inf.br>
2024-09-07 15:23:58 -0300

    add health check

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+export default async function server() {
+    const app = express();
+    app.get("/health", (req, res) => {
+        res.status(200).send("OK");
+    });
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Health check running on port ${port}`);
+    });
+}
