612a76803012de2c22b56b2f9760f6e5637e1d78
Wiliam Joaquim <wiliam.joaquim@tempocerto.inf.br>
2024-09-11 21:08:22 -0300

    feat: add health check

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"log/slog"
+	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/wiliamvj/go-vagas/internal/bot"
@@ -15,6 +16,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	go func() {
+		http.HandleFunc("/health", bot.HealthCheck)
+		slog.Info("Starting health check server on :8080")
+
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal("Failed to start health check server:", err)
+		}
+	}()
 
 	err = bot.Websocket()
 	if err != nil {
